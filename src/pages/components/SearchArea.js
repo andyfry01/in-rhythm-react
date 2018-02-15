6719b330d598b6f915cfd3535f232d7d8d930f38
Andy Fry <andyfry01@gmail.com>
2018-02-14 20:29:29 -0500

    feat: add search area layout, basic UI
    adde search area layout and styling for mobile, added basic UI for tracking user input state and
    clearing input state

--- src/pages/components/SearchArea.js
+++ src/pages/components/SearchArea.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import icon from "../../assets/slash.svg";
 
 class SearchArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ""
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.clearSearchTerm = this.clearSearchTerm.bind(this);
+  }
+
+  handleInput(e) {
+    const searchTerm = e.target.value;
+    this.setState((state, props) => {
+      return { searchTerm: searchTerm };
+    });
+  }
+
+  clearSearchTerm() {
+    this.setState((state, props) => {
+      return { searchTerm: "" };
+    });
+  }
+
   render() {
     return (
-     <h1>Hello from SearchArea</h1>
+      <div className="searchArea">
+        <input
+          className="searchArea__input"
+          type="text"
+          placeholder="enter an artist"
+          value={this.state.searchTerm}
+          onChange={e => this.handleInput(e)}
+        />
+        <button
+          className="searchArea__clearInput"
+          onClick={this.clearSearchTerm}
+        >
+          <img
+            className="searchArea__clearInputIcon"
+            src={icon}
+            alt="clear search icon"
+          />
+        </button>
+      </div>
     );
   }
 }
