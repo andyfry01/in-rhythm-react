8fdb07016a97bc609f047195ac0e85562cb009b4
Andy Fry <andyfry01@gmail.com>
2018-02-16 02:30:31 -0500

    refactor: change header image
    fun with random images

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
-import testImg from "../../assets/test-img.jpg";
+import turntable from "../../assets/turntable.jpeg";
+import albums from "../../assets/albums.jpg";
+import casette from "../../assets/casette.jpg";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <div className="header__lowerLayer">
-          <div className="header__image">
-            <img alt="header image" src={testImg} />
-          </div>
-          <div className="header__gradient"></div>
+const Header = () => {
+  const images = [turntable, albums, casette]
+  const randInt = Math.floor(Math.random() * (2 - 0 + 1)) + 0
+  const headerImg = images[randInt]
+  return (
+    <header className="header">
+      <div className="header__lowerLayer">
+        <div className="header__image">
+          <img alt="header image" src={headerImg} />
         </div>
-        <div className="header__upperLayer">
-          <div className="header__textBox">
-            <h1 className="header__headerText">In Rhythm <br /> Album Browser</h1>
-          </div>
+        <div className="header__gradient"></div>
+      </div>
+      <div className="header__upperLayer">
+        <div className="header__textBox">
+          <h1 className="header__headerText">In Rhythm <br /> Album Browser</h1>
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
 }
 
 export default Header;
