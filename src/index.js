48f7a3aeced72c66178ef5fbb0e85f8d3f702b48
Andy Fry <andyfry01@gmail.com>
2018-02-15 22:06:52 -0500

    feat: add redux to index.js
    added redux to index.js so the app has access to store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createLogger } from 'redux-logger'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import reducer from './reducers/reducer'
+
+import registerServiceWorker from './registerServiceWorker';
+const loggerMiddleware = createLogger()
+
+
+let store = createStore(reducer, applyMiddleware(thunkMiddleware, loggerMiddleware))
+
+ReactDOM.render(
+  <Provider store={store}> 
+    <App /> 
+  </Provider>, document.getElementById('root'));
+
 registerServiceWorker();
